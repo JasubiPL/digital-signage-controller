7b274cca67247edc01ceb4783bee841eab272e61
Jasubi Piñeyro <gameportal7@hotmail.com>
2024-03-11 17:51:00 -0600

    Se renderiza la lista de taquillas y se agregan botones de accion 'ver, editar y borrar'

--- Interface/src/pages/BoxOfficePage.tsx
+++ Interface/src/pages/BoxOfficePage.tsx
@@ -1,28 +1,41 @@
 import { useEffect, useState } from "react"
+import { FaRegEdit } from "react-icons/fa";
 import { getInfoDb } from "../helpers/getInfoDB"
+import { MdOutlineDeleteForever } from "react-icons/md";
+import { GrFormView } from "react-icons/gr";
+import { useLocation } from "react-router-dom";
+import { AddBoxOffice } from "../users/admin/components";
+
+interface Office {
+  nombre: string;
+  // otros campos necesarios
+}
+
+interface BoxOffice {
+  nombre: string,
+  dispositivo: string,
+  proyeccion: string,
+  estatus: string
+  // otras propiedades si las hay
+}
 
 
 export const BoxOfficePage = () =>{
 
-  const [allBoxOffice, setAllBoxOffice] = useState([ 'All'])
-  const [selectOffice, setSelectOffice] = useState([ 'All'])
-  const [currentBoxOffice, setCurrentBoxOffice] = useState("All")
+  const [allBoxOffice, setAllBoxOffice] = useState<BoxOffice[]>([])
+  const [modal, setModal] = useState<JSX.Element | null>(null);
 
-  console.log(currentBoxOffice)
+  const currentPath = useLocation()
+
+  //console.log(currentBoxOffice)
 
   const setQuery = async () =>{
-    const selectQueryData =  await getInfoDb('taquillas', 'ETN')
-    setSelectOffice(selectQueryData)
 
-    if(currentBoxOffice === "All"){
-      const queryData =  await getInfoDb('taquillas', 'ETN')
-      
-      //enviamos a la lista de seleccion las taquillas existentes
-      setAllBoxOffice(queryData)
+    const queryData =  await getInfoDb('taquillas', 'ETN')
+    
+    //enviamos a la lista de seleccion las taquillas existentes
+    setAllBoxOffice(queryData)
 
-    }else{
-      setAllBoxOffice(['TODO => Crear la query'])
-    }
 
 
     //TODO - Aqui iria la query por taquilla
@@ -33,24 +46,22 @@ export const BoxOfficePage = () =>{
 
     setQuery()
 
-  }, [currentBoxOffice])
+  }, [])
 
 
   
 
   return(
     <section className="w-full  h-full flex flex-col items-center overflow-y-auto my-8">
-      <select 
-        className=" self-end mr-[5%] bg-red-600 text-white text-xl"
-        onChange={(e) => setCurrentBoxOffice(e.target.value)}
-      >
-      <option key='All' value="All">Todas las taquillas</option>
-      {
-        selectOffice.map( option =>(
-          <option key={option} value={option}>{option}</option>
-        ))
-      }
-      </select>
+      { modal }
+      <section className="w-[90%] flex justify-between mb-4">
+        <button 
+        onClick={() => setModal(<AddBoxOffice />)}
+          className="py-1 px-8 bg-red-600 text-white text-xl hover:scale-105 active:scale-90 transition-all"
+        >
+          Agregar Taquilla +
+        </button>
+      </section>
       <section className="w-[90%] pt-4 bg-white mt-2 border-b-[1px] border-gray-200">
         <header className="text-center grid grid-cols-5 font-semibold border-b-[1px] border-gray-200 pb-1">
           <article>Taquilla ▾</article>
@@ -61,13 +72,29 @@ export const BoxOfficePage = () =>{
         </header>
         {
           allBoxOffice.map( office =>(
-            <div className="text-center grid grid-cols-5 border-b-[1px] border-gray-200 pb-1">
-              <div className=" text-left pl-4">{ office }</div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div>
-
+            <div key={office.nombre} className="text-center grid grid-cols-5 border-b-[1px] border-gray-200 py-2">
+              <div className=" text-left pl-4">{ office.nombre }</div>
+              <div>{ office.dispositivo }</div>
+              <div>{ office.proyeccion }</div>
+              <div><span className={office.estatus  != 'dañada' ?  office.estatus : 'dañada'}>{ office.estatus }</span></div>
+              <div className="flex gap-4 justify-center">
+                <button className="hover:scale-110 active:scale-90 transition-all hover:after:content-['Ver'] 
+                after:absolute after:bg-gray-900 after:px-2 after:text-white after:top-[-20px] after:left-0">
+                  <GrFormView className="text-4xl text-blue-500"/>
+                </button>
+                <button className="hover:scale-110 active:scale-90 transition-all hover:after:content-['Editar'] 
+                after:absolute after:bg-gray-900 after:px-2 after:text-white after:top-[-20px] after:left-0">
+                  <FaRegEdit className="text-2xl text-orange-400"/>
+                </button>
+                {
+                  //comprovamos si estamos en la ruta del Administrador para mostrar el boton de borrado
+                  currentPath.pathname.includes('admin') 
+                  ? <button className="hover:scale-110 active:scale-90 transition-all hover:after:content-['Eliminar'] 
+                  after:absolute after:bg-gray-900 after:px-2 after:text-white after:top-[-20px] after:left-0">
+                      <MdOutlineDeleteForever className="text-3xl text-red-600"/>
+                    </button>
+                  : ""
+                }
               </div>
             </div>
           ))
---
+++ Interface/src/users/admin/components/AddBoxOffice.tsx
@@ -0,0 +1,7 @@
+export const AddBoxOffice = () =>{
+  return(
+    <section className="w-screen h-screen bg-black/50 absolute top-0 left-0">
+
+    </section>
+  )
+} 
