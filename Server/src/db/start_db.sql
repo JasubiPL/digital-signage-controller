b555225224373062f229616d86ad9fd4728e9ce1
Jasubi Piñeyro <gameportal7@hotmail.com>
2024-03-11 15:37:59 -0600

    Se Agrega data a la tabla de taquillas, se insertan 3 nuevas columnas y se cambia la Api para postrar las nuevas columnas

--- Server/src/db/start_db.sql
+++ Server/src/db/start_db.sql
@@ -10,14 +10,18 @@ USE digital_signage_db;
 -- Creamos las tablas
 CREATE TABLE IF NOT EXISTS taquillas_ETN (
 	id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(255) NOT NULL
+    nombre VARCHAR(255) NOT NULL,
+    dispositivo VARCHAR(50) NOT NULL,
+    proyeccion VARCHAR(50) NOT NULL,
+    estatus VARCHAR(50) NOT NULL
+    
 );
 
 CREATE TABLE IF NOT EXISTS campañas_ETN (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(255) NOT NULL UNIQUE,
-    fecha_inicio VARCHAR(9),
-    fecha_fin VARCHAR(9),
+    fecha_inicio VARCHAR(15),
+    fecha_fin VARCHAR(15),
     estatus VARCHAR(50)
 );
 
@@ -30,29 +34,6 @@ CREATE TABLE taquillas_campañas_ETN(
 
 show tables;
 
--- Insertamos valores iniciales en la tabla taquillas
-INSERT INTO taquillas_ETN (nombre)
-VALUES 
-('Mexico Norte'), ('Mexico Sur'), ('Mexico Poniente'), ('Acapulco Diamante'), ('Acapulco Papagayo'),
-('Aguascalientes'), ('Leon'), ('Tlaquepaque'), ('Celaya'), ('Colima'),
-('Mexico Cuernavaca'), ('Culiacan'), ('Durango'), ('Guadalajara'), ('Guanajuato'),
-('Irapuato'), ('Manzanillo'), ('Matamoros'), ('Mazatlan'), ('Monterrey'),
-('Morelia'), ('Nayarit'), ('Nuevo Laredo'), ('Puerto Vallarta'), ('Queretaro'),
-('Los Mochis'), ('San Juan del Rio'), ('San Luis Potosi'), ('Santiago'), ('Plaza Sendero'),
-('Tampico'), ('Tepic'), ('Tepotzotlan'), ('Toluca'), ('Torreon'),
-('Uruapan'), ('Zacatecas'), ('Zamora'), ('Zapopan');
-
-
-
--- Insertamos las campañas Olways On
-INSERT INTO campañas_ETN (nombre, fecha_inicio, fecha_fin, estatus)
-VALUES 
-('Términos y condiciones', ' ', 'OLWAYS ON', 'activo'),
-('No discriminacion', ' ', 'OLWAYS ON', 'activo'),
-('Identificacion Oficial', ' ', 'OLWAYS ON', 'activo'),
-('Aviso de Privacidad', ' ', 'OLWAYS ON', 'activo'),
-('Aviso migratorio', ' ', 'OLWAYS ON', 'activo');
-
 -- Insertamos relacion
 -- Ejemplo de insercion
 -- INSERT INTO taquillas_campañas_ETN(taquilla_id, campaña_id) VALUES ((SELECT id FROM taquillas_ETN WHERE nombre = 'Mexico Norte'),(SELECT id FROM campañas_ETN WHERE nombre = 'No discriminacion'));
@@ -62,6 +43,7 @@ SELECT * FROM taquillas_ETN;
 SELECT * FROM campañas_ETN;
 SELECT * FROM taquillas_campañas_ETN;
 
+-- Ejemplo de Consulta
 SELECT taquillas_ETN.nombre AS 'Nombre de Taquilla',
        campañas_ETN.nombre AS 'Nombre de Campaña',
        campañas_ETN.fecha_inicio AS 'Fecha de Inicio',
