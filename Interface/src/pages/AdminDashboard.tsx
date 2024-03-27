2a6e7dbcdfa3b71a7982442b230010912f7ffe4a
Jasubi Piñeyro <gameportal7@hotmail.com>
2024-03-27 10:45:13 -0600

    [Interface]: Se crean las cards del dashboard con el numero de taquillas

--- Interface/src/pages/AdminDashboard.tsx
+++ Interface/src/pages/AdminDashboard.tsx
@@ -1,27 +1,22 @@
 import { useEffect, useState } from "react"
 import { getInfoDb } from "../helpers/getInfoDB"
-import { AiOutlineUsb } from "react-icons/ai"
-import { BsDeviceSsd } from "react-icons/bs"
+import { BoxOfficeMetrix } from "../ui"
 
-interface BoxOffice {
-  id: string,
-  nombre: string,
-  dispositivo: string,
-  proyeccion: string,
-  estatus: string
-}
+
 
 export const AdminDashboard = () =>{
 
-  const [etnBoxOffice, setEtnBoxOffice] = useState<BoxOffice[]>([])
-  const [ghoBoxOffice, setGhoBoxOffice] = useState<BoxOffice[]>([])
-  const [costaBoxOffice, setCostaBoxOffice] = useState<BoxOffice[]>([])
-
-  console.log(etnBoxOffice.filter(office => office.proyeccion === "PLAYER"))
+  const [etnBoxOffice, setEtnBoxOffice] = useState([])
+  const [ghoBoxOffice, setGhoBoxOffice] = useState([])
+  const [costaBoxOffice, setCostaBoxOffice] = useState([])
 
   const getBoxOffice = async () =>{
     const dataETN =  await getInfoDb('taquillas', 'ETN')
+    const dataGHO =  await getInfoDb('taquillas', 'GHO')
+    const dataCosta =  await getInfoDb('taquillas', 'COSTA')
     setEtnBoxOffice(dataETN)
+    setGhoBoxOffice(dataGHO)
+    setCostaBoxOffice(dataCosta)
 
     //TODO Peticiones a gho y COsta
 
@@ -33,18 +28,18 @@ export const AdminDashboard = () =>{
 
   }, [])
   return(
-    <section className="w-full h-full flex justify-center items-center">
-      <section className="w-[95%] h-[95%]">
-        <div className="w-1/5 bg-white py-4 px-8">
-          <p className="text-xl text-red-600">Taquillas ETN</p>
-          <div className="flex justify-between mt-2 items-center">
-            <p className="text-6xl font-semibold">{etnBoxOffice.length}</p>
-            <div>
-              <p className="flex gap-2 justify-between text-md"> Player: { etnBoxOffice.filter(office => office.proyeccion === "PLAYER").length } <span className="text-red-600"> <BsDeviceSsd /> </span></p>
-              <p className="flex gap-2 justify-between text-md">USB: { etnBoxOffice.filter(office => office.proyeccion === "USB").length } <span className="text-red-600"><AiOutlineUsb /></span></p>
-            </div>
-          </div>
+    <section className="w-full max-h-full flex pt-8 justify-center items-center">
+      <section className="w-[95%] h-[95%] flex gap-4">
+        <div className="w-full flex gap-4">
+          <BoxOfficeMetrix boxOffice={etnBoxOffice} title="Taquillas ETN"/>
+          <BoxOfficeMetrix boxOffice={ghoBoxOffice} title="Taquillas GHO"/>
+          <BoxOfficeMetrix boxOffice={costaBoxOffice} title="Taquillas COSTA"/>
         </div>
+        <div>
+          
+        </div>
+
+
       </section>
     </section>
   )
---
+++ Interface/src/ui/components/BoxOfficeMetrics.tsx
@@ -0,0 +1,33 @@
+import { FC } from "react"
+import { AiOutlineUsb } from "react-icons/ai"
+import { BsDeviceSsd } from "react-icons/bs"
+
+interface BoxOffice {
+  id: string,
+  nombre: string,
+  dispositivo: string,
+  proyeccion: string,
+  estatus: string
+}
+
+
+interface Props {
+  boxOffice: BoxOffice[],
+  title: string
+}
+
+
+export const BoxOfficeMetrix: FC<Props> = ({boxOffice, title }) =>{
+  return(
+    <div className="w-1/5 bg-white py-4 px-8">
+      <p className="text-xl text-red-600">{title}</p>
+      <div className="flex justify-between mt-2 items-center">
+        <p className="text-6xl font-semibold">{boxOffice.length}</p>
+        <div>
+          <p className="flex gap-2 justify-between text-md"> Player: { boxOffice.filter(office => office.proyeccion === "PLAYER").length } <span className="text-red-600"> <BsDeviceSsd /> </span></p>
+          <p className="flex gap-2 justify-between text-md">USB: { boxOffice.filter(office => office.proyeccion === "USB").length } <span className="text-red-600"><AiOutlineUsb /></span></p>
+        </div>
+      </div>
+    </div>
+  )
+}
