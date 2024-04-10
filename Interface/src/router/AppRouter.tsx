a8b211fc6d92ab967fb91451b3783e23d65ebbbe
Jasubi Piñeyro <gameportal7@hotmail.com>
2024-04-10 13:52:13 -0600

    [Interface]: Se crean rutas publicas y privadas

--- Interface/src/router/AppRouter.tsx
+++ Interface/src/router/AppRouter.tsx
@@ -3,16 +3,36 @@ import { AdminRoutes } from "../users/admin/routes/AdminRoutes"
 import { ManagerRoutes } from "../users/manager/routes/ManagerRutes"
 import { DesignersRoutes } from "../users/designers/routes/DesignersRoutes"
 import { LoginPage, HomePage } from "../pages"
+import { PrivateRoutes } from "./PrivateRutes"
+import { PublicRoutes } from "./PublicRoutes"
 
 export const AppRouter = () =>{
   return(
     <Routes>
+      <Route path="/login" element={
+        <PublicRoutes>
+          <LoginPage />
+        </PublicRoutes>
+      } />
+
+      
       <Route path="/*" element={<Navigate to='err404' />} />
       <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="admin/*" element={<AdminRoutes />} />
-      <Route path="manager/*" element={<ManagerRoutes />} />
-      <Route path="designers/*" element={<DesignersRoutes />} />
+      <Route path="admin/*" element={
+        <PrivateRoutes>
+          <AdminRoutes />
+        </PrivateRoutes>
+      } />
+      <Route path="manager/*" element={
+        <PrivateRoutes>
+          <ManagerRoutes />
+        </PrivateRoutes>
+      } />
+      <Route path="designers/*" element={
+        <PrivateRoutes>
+          <DesignersRoutes />
+        </PrivateRoutes>
+      } />
     </Routes>
 
   )
